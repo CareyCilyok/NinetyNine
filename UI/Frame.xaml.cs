75f579da32fce7d2c13cddfc11dfd62158b035ec
Carey Cilyok <CareyCilyok@gmail.com>
2019-11-25 18:05:01 -0600

    Adding the XAML and code behind

---
+++ UI/Frame.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NinetyNine.UI
+{
+    /// <summary>
+    /// Interaction logic for Frame.xaml
+    /// </summary>
+    public partial class Frame : UserControl
+    {
+        public Frame()
+        {
+            InitializeComponent();
+        }
+    }
+}
