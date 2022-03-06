aec9a53615b2c0df25c1c87f246c5f47605d7798
kyleworley <kyle@k-w.xyz>
2022-03-05 17:36:50 -0700

    Adds a new avalonia presentation project
    Didn't want to remove the old presentation project at the momement, so
    I added in a new presentation project for the avalonia UI work.

---
+++ Avalonia.Presentation/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Avalonia.Presentation.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
