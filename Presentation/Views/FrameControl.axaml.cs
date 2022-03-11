767eae088d8d82b9436b56fe404b0e27979e208a
CareyCilyok <CareyCilyok@Gmail.com>
2022-03-10 22:29:40 -0600

    Mocked up wireframe of the FrameControl.

---
+++ Presentation/Views/FrameControl.axaml.cs
@@ -0,0 +1,10 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+
+namespace NinetyNine.Presentation.Views
+{
+	public class FrameControl : TemplatedControl
+	{
+	}
+}
